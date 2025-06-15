2faff3485be2d66a2d8cb9f101d2d0bce1b1457c
Adityakbr01 <aditykbr01@gmail.com>
2025-06-15 12:49:47 +0530

    git commit -m "feat(app): add AppInitializer in App.tsx to preload user data from API"

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,10 @@
 import RoutingLayout from "@/layouts/RoutingLayout";
 import useSessionWatcher from "./auth/useSessionWatcher";
+import AppInitializer from "./components/AppInitializer";
 
 function App() {
    useSessionWatcher();
+   AppInitializer()
   return <RoutingLayout />;
 }
 
